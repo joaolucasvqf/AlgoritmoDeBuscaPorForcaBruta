0183449364ea803d4897f61aac6519cc2efb5f89
João Lucas Fonseca <joao.fonseca@csistemas.com.br>
2020-09-25 10:32:21 -0300

    Refatoração do código

--- BuscaPorForcaBruta/Program.cs
+++ BuscaPorForcaBruta/Program.cs
@@ -10,13 +10,12 @@ namespace BuscaPorForcaBruta
             string repetir = "0";
             do
             {
-
                 int[] arr = new int[1000];
                 int count = 0;
                 Array.Clear(arr, 0, 1000);
                 Random r = new Random();
-                pessoa p1 = new pessoa() { posicao = r.Next(0, 999), direcao = r.Next(0, 999) < 500 };
-                pessoa p2 = new pessoa() { posicao = r.Next(0, 999), direcao = r.Next(0, 999) < 500 };
+                pessoa p1 = new pessoa() { id = 1, posicao = r.Next(0, 999), direcao = r.Next(0, 999) < 500 };
+                pessoa p2 = new pessoa() { id = 2, posicao = r.Next(0, 999), direcao = r.Next(0, 999) < 500 };
                 Console.WriteLine($"A pessoa 1 iniciou na posição {p1.posicao}");
                 Console.WriteLine($"A pessoa 2 iniciou na posição {p2.posicao}");
                 Console.WriteLine("Pressione enter para iniciar!");
@@ -24,99 +23,8 @@ namespace BuscaPorForcaBruta
 
                 while (p1.posicao != p2.posicao)
                 {
-                    if (p1.SentiuOCheiroDoCouro)
-                    {
-                        if (p1.direcao)
-                        {
-                            if (p1.posicao + 1 < arr.Length)
-                                p1.posicao++;
-                        }
-                        else
-                        {
-                            if (p1.posicao - 1 > 0)
-                                p1.posicao--;
-                        }
-                    }
-                    else
-                    {
-                        if (p1.direcao)
-                        {
-                            if (p1.posicao + 1 > arr.Length - 1)
-                            {
-                                p1.posicao--;
-                                p1.direcao = false;
-                            }
-                            else if (arr[p1.posicao + 1] == 0)
-                            {
-                                p1.direcao = false;
-                                p1.posicao++;
-                            }
-                            else
-                                p1.posicao++;
-                        }
-                        else
-                        {
-                            if (p1.posicao - 1 <= 0)
-                            {
-                                p1.posicao++;
-                                p1.direcao = true;
-                            }
-                            else if (arr[p1.posicao - 1] == 0)
-                            {
-                                p1.direcao = true;
-                                p1.posicao--;
-                            }
-                            else
-                                p1.posicao--;
-                        }
-
-                    }
-                    if (p2.SentiuOCheiroDoCouro)
-                    {
-                        if (p2.direcao)
-                        {
-                            if (p2.posicao + 1 < arr.Length)
-                                p2.posicao++;
-                        }
-                        else
-                        {
-                            if (p2.posicao - 1 > 0)
-                                p2.posicao--;
-                        }
-                    }
-                    else
-                    {
-                        if (p2.direcao)
-                        {
-                            if (p2.posicao + 1 > arr.Length - 1)
-                            {
-                                p2.posicao--;
-                                p2.direcao = false;
-                            }
-                            else if (arr[p2.posicao + 1] == 0)
-                            {
-                                p2.direcao = false;
-                                p2.posicao++;
-                            }
-                            else
-                                p2.posicao++;
-                        }
-                        else
-                        {
-                            if (p2.posicao - 1 <= 0)
-                            {
-                                p2.posicao++;
-                                p2.direcao = true;
-                            }
-                            else if (arr[p2.posicao - 1] == 0)
-                            {
-                                p2.direcao = true;
-                                p2.posicao--;
-                            }
-                            else
-                                p2.posicao--;
-                        }
-                    }
+                    AndarUmaCasa(arr, p1);
+                    AndarUmaCasa(arr, p2);
 
                     if (p1.direcao)
                         Console.WriteLine("P1 andando para a direita!");
@@ -127,27 +35,88 @@ namespace BuscaPorForcaBruta
                     else
                         Console.WriteLine("P2 andando para a esquerda!");
 
-
-                    if (arr[p1.posicao] == 0)
-                        arr[p1.posicao] = 1;
-                    else if (arr[p1.posicao] == 2)
-                        p1.SentiuOCheiroDoCouro = true;
-                    if (arr[p2.posicao] == 0)
-                        arr[p2.posicao] = 2;
-                    else if (arr[p2.posicao] == 1)
-                        p2.SentiuOCheiroDoCouro = true;
+                    MarcarPosicao(arr, p1);
+                    MarcarPosicao(arr, p2);
                     count++;
+
+                    if (count > 1000000)
+                    {
+                        Console.WriteLine("Não foi encontrada uma resposta válida no intervalo de iterações definido!");
+                        Console.WriteLine($"Foram realizadas {count} iterações!");
+                        break;
+                    }
                 }
-                Console.WriteLine($"As pessoinhas se encontraram na posicao {p1.posicao}!");
-                Console.WriteLine($"Foram necessárias {count} iterações!");
+                if (p1.posicao == p2.posicao)
+                {
+                    Console.WriteLine($"As pessoinhas se encontraram na posicao {p1.posicao}!");
+                    Console.WriteLine($"Foram necessárias {count} iterações!");
+                }
                 Console.WriteLine("Caso deseje realizar mais uma simulação, digite 1, caso contrario, digite 0!");
                 repetir = Console.ReadLine();
-            } while (repetir == "1");
+            } while (repetir != "0");
+        }
+        private static void AndarUmaCasa(int[] arr, pessoa p)
+        {
+            if (p.SentiuOCheiroDoCachorro)
+            {
+                if (p.direcao)
+                {
+                    if (p.posicao + 1 < arr.Length)
+                        p.posicao++;
+                }
+                else
+                {
+                    if (p.posicao - 1 > 0)
+                        p.posicao--;
+                }
+            }
+            else
+            {
+                if (p.direcao)
+                {
+                    if (p.posicao + 1 >= arr.Length - 1)
+                    {
+                        p.posicao--;
+                        p.direcao = false;
+                    }
+                    else if (arr[p.posicao + 1] == 0)
+                    {
+                        p.posicao++;
+                        p.direcao = false;
+                    }
+                    else
+                        p.posicao++;
+                }
+                else
+                {
+                    if (p.posicao - 1 <= 0)
+                    {
+                        p.posicao++;
+                        p.direcao = true;
+                    }
+                    else if (arr[p.posicao - 1] == 0)
+                    {
+                        p.posicao--;
+                        p.direcao = true;
+                    }
+                    else
+                        p.posicao--;
+                }
+
+            }
+        }
+        private static void MarcarPosicao(int[] arr, pessoa p)
+        {
+            if (arr[p.posicao] == 0)
+                arr[p.posicao] = p.id;
+            else if (arr[p.posicao] != p.id)
+                p.SentiuOCheiroDoCachorro = true;
         }
         class pessoa
         {
+            public int id { get; set; }
             public int posicao { get; set; }
-            public bool SentiuOCheiroDoCouro { get; set; } = false;
+            public bool SentiuOCheiroDoCachorro { get; set; } = false;
             public bool direcao { get; set; }
         }
     }
